bbf32a7dadbd552cc4f9e58974a07baeb5a0973f
Gio <gio@webstarts.com>
2016-06-08 18:14:24 -0400

    Added Resource Controllers

--- app/Http/Controllers/Api/PromotionFeedController.php
+++ app/Http/Controllers/Api/PromotionFeedController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use Promotion;
+
 class PromotionFeedController extends Controller
 {
     /**
@@ -16,7 +18,7 @@ class PromotionFeedController extends Controller
      */
     public function index()
     {
-        //
+        return "List of All post";
     }
 
     /**
